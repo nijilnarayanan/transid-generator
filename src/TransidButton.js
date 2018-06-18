ad42c540f007132d69636a0554339033b0148a44
Nijil Narayanan <nijil.narayanan@BIGTREE.ORG>
2018-06-18 20:20:21 +0530

    updated

--- src/TransidButton.js
+++ src/TransidButton.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './TransidButton.css'
 
 class TransidButton extends Component {
+
+handleClick(e){
+e.preventDefault();
+}
   render() {
     return (
         <div>
-                <button className='button'>Generate Transaction ID</button>
+                <button className='button' onClick={this.handleClick}>Generate Transaction ID</button>
         </div>
     );
   }
